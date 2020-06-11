32cd2a4d1e2a6797cc6c0a68603626452220071b
Никита Федосеев <zveri132@gmail.com>
2020-06-11 17:39:38 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Покупатель
+    {
+        public string FirstName;
+        public string LastName;
+        public string Otchestvo;
+        protected string Address;
+        private int BankCard;
+        private int BankAcc;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Покупатель p1 = new Покупатель();
+            p1.FirstName = "Андрей";
+            p1.LastName = "Андреев";
+            p1.Otchestvo = "Андреевич";
+            
+            Console.WriteLine(p1.FirstName);
+            Console.WriteLine(p1.LastName);
+            Console.WriteLine(p1.Otchestvo);
+            Console.ReadKey();
+        }
+    }
+}
